c408cbf6cb203991e4e35446724bf68bfa0d6d38
Łukasz Kurowski <crackcomm@gmail.com>
2018-12-07 23:45:12 +0100

    chore(*): license fixes

--- sealbox/sealed.go
+++ sealbox/sealed.go
@@ -1,21 +1,18 @@
 // Copyright © 2017-2018 The IPFN Developers. All Rights Reserved.
 // Copyright © 2014-2018 The go-ethereum Authors. All Rights Reserved.
 //
-// This file is part of the IPFN project.
-// This file was part of the go-ethereum library.
-//
-// The IPFN project is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The IPFN project is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // Package sealbox implements web3 secrets storage.
 //
